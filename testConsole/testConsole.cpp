e856129a4e33312b37867580aefe5efdb1ddaad2
Nash <yu_shaona@163.com>
2019-06-24 21:05:16 +0800

    11

--- testConsole/testConsole.cpp
+++ testConsole/testConsole.cpp
@@ -14,14 +14,35 @@
 
 #ifdef _DEBUG
 #pragma comment(lib,"libuv_d.lib")
+#pragma comment(lib,"..\\lib\\log4z_win32d.lib")
 #else
 #pragma comment(lib,"libuv.lib")
 #endif
 
 #include "uv.h"
+#include "..\include\log4z\log4z.h"
+
+using namespace zsummer::log4z;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	
+	ILog4zManager::getRef().setLoggerPath(LOG4Z_MAIN_LOGGER_ID, "./log2");
+	ILog4zManager::getRef().start();
+	ILog4zManager::getRef().setLoggerLevel(LOG4Z_MAIN_LOGGER_ID, LOG_LEVEL_TRACE);
+	//LOGD: LOG WITH level LOG_DEBUG
+	//LOGI: LOG WITH level LOG_INFO
+
+	//begin test stream log input....
+	LOGT("stream input *** " << "LOGT LOGT LOGT LOGT" << " *** ");
+	LOGD("stream input *** " << "LOGD LOGD LOGD LOGD" << " *** ");
+	LOGI("stream input *** " << "LOGI LOGI LOGI LOGI" << " *** ");
+	LOGW("stream input *** " << "LOGW LOGW LOGW LOGW" << " *** ");
+	LOGE("stream input *** " << "LOGE LOGE LOGE LOGE" << " *** ");
+	LOGA("stream input *** " << "LOGA LOGA LOGA LOGA" << " *** ");
+	LOGF("stream input *** " << "LOGF LOGF LOGF LOGF" << " *** ");
+
+
 	uv_loop_t *loop = (uv_loop_t *)malloc(sizeof(uv_loop_t));
 	uv_loop_init(loop);
 
